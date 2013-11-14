26f9e6141846d14e4cd3822911b104d1ace44c4c
raphdom <raphdom@gmai.com>
2013-11-14 16:37:21 +0000

    register dialog

--- WebContent/app/view/authentication/Register.js
+++ WebContent/app/view/authentication/Register.js
@@ -20,56 +20,77 @@ Ext.define('AboutUs.view.authentication.Register', {
     
     plugins:['validationMessages'],
     
-    items: [
-        {
-            allowBlank: false,
-            fieldLabel: 'Email',
-            name: 'email',
-            emptyText: 'Email'
-        },
-        {
-            allowBlank: false,
-            fieldLabel: 'Palavra-chave',
-            name: 'password',
-            emptyText: 'Palavra-chave',
-            inputType: 'password'
-        },
-        {
-        	allowBlank: false,
-            fieldLabel: 'Igreja',
-            name: 'churchName'
-        },
-        {
-        	allowBlank: false,
-            fieldLabel: 'Morada da Igreja',
-            name: 'churchAddress'
-        },
-        {
-        	allowBlank: false,
-            fieldLabel: 'País',
-            xtype:'combo',
-            store:'list.CountryStore',
-            valueField:'value',
-            forceSelection:true,
-            displayField:'text',
-            name: 'country',
-            queryMode:'local'
-        },
-        {
-        	allowBlank: false,
-            fieldLabel: 'Nome do Responsável',
-            name: 'nameResp'
-        },
-        {
-        	allowBlank: false,
-            fieldLabel: 'Nome do site',
-            name: 'siteAlias'
-        },
-        {
-            fieldLabel: 'Quantos membro tem sua igreja?',
-            name: 'qtdMembers'
-        }
-    ],
+    items: [{
+    		xtype:'fieldset',
+            title: 'Dados da Igreja',
+            collapsible: false,
+            defaultType: 'textfield',
+            layout: 'anchor',
+            defaults: {
+                anchor: '100%'
+            },
+            items:[{
+		        	allowBlank: false,
+		            fieldLabel: 'Igreja',
+		            name: 'churchName'
+		        },
+		        {
+		        	allowBlank: false,
+		            fieldLabel: 'Morada da Igreja',
+		            name: 'churchAddress'
+		        },
+		        {
+		        	allowBlank: false,
+		            fieldLabel: 'País',
+		            xtype:'combo',
+		            store:'list.CountryStore',
+		            valueField:'value',
+		            forceSelection:true,
+		            displayField:'text',
+		            name: 'country',
+		            queryMode:'local'
+		        },{
+		        	allowBlank: false,
+		            fieldLabel: 'Nome do site',
+		            name: 'siteAlias'
+		        },
+		        {
+		            fieldLabel: 'Quantos membro tem sua igreja?',
+		            name: 'qtdMembers'
+		        }]
+    	},{
+			xtype:'fieldset',
+	        title: 'Dados de Acesso',
+	        collapsible: false,
+	        defaultType: 'textfield',
+	        layout: 'anchor',
+	        defaults: {
+	            anchor: '100%'
+	        },
+	        items:[{
+		            allowBlank: false,
+		            fieldLabel: 'Email',
+		            name: 'email',
+		            emptyText: 'Email',
+		            vtype:'email'
+		        },
+		        {
+		            allowBlank: false,
+		            fieldLabel: 'Palavra-chave',
+		            name: 'password',
+		            emptyText: 'Palavra-chave',
+		            inputType: 'password'
+		        },{
+		        	allowBlank: false,
+		            fieldLabel: 'Confirmar palavra-chave',
+		            name: 'passwordConfirm',
+		            inputType: 'password'
+		        },{
+		        	allowBlank: false,
+		            fieldLabel: 'Nome completo',
+		            name: 'nameResp'
+		        }]
+	}],
     
     buttons: [{ 
         	text:'Registar',
