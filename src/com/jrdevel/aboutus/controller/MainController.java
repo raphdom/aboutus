13f609701615309b33438bc6e08ec698037354ec
raphdom <raphdom@gmail.com>
2013-11-18 20:31:55 +0000

    user session

--- src/com/jrdevel/aboutus/controller/MainController.java
+++ src/com/jrdevel/aboutus/controller/MainController.java
@@ -2,9 +2,12 @@ package com.jrdevel.aboutus.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.jrdevel.aboutus.model.User;
 
 /**
  * @author Raphael Domingues
@@ -13,10 +16,14 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MainController {
 	
+	
+	@Autowired
+	private User userSession;
+	
 	@RequestMapping(value="/home.action")
 	public ModelAndView home(HttpSession session) throws Exception {
 		
-		if (session.getAttribute("user")==null){
+		if (userSession.getId()==null){
 			return new ModelAndView("login");
 		}else{
 			return new ModelAndView("home");
